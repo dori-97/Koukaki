6f4d0e605604c7aa208e1d3775d24e4a998e653d
Doriane Lucas <doriane.mlcs@gmail.com>
2024-09-30 11:59:52 +0200

    modification fade in/fade down

--- js/main.js
+++ js/main.js
@@ -2,10 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('.section'); // Sélectionne toutes les sections
 
-    // Boucle à travers chaque section
-    sections.forEach((section, index) => {
-        setTimeout(() => {
-            section.classList.add('section-visible'); // Ajoute la classe 'visible' pour déclencher l'effet fade in
-        }, index * 500); // Délai de 500ms entre chaque section
+    sections.forEach((section) => {
+        section.classList.add('section-visible');
     });
 });
