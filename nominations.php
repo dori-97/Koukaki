308d247856321d8d1dde7e2850b41115718f20f4
Doriane Lucas <doriane.mlcs@gmail.com>
2024-10-04 12:17:19 +0200

    étape 1 : création section oscars

---
+++ nominations.php
@@ -0,0 +1,8 @@
+<section id="oscars" class="section">
+       <div class="oscars-container">
+           <div class="OscarsTitleContainer">
+               <h3 class="oscars-title">Fleurs d’oranger & chats errants<span> est nominé aux Oscars </span>Short Film Animated de 2022 !</h3>
+           </div>
+           <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/oscars.png'; ?>">
+       </div>
+   </section>
